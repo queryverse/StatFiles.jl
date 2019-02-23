4d8bea6150495ef44c6879ea26471ef413bea3a5
David Anthoff <anthoff@berkeley.edu>
2019-02-23 12:41:20 -0800

    Add docs template

---
+++ docs/make.jl
@@ -0,0 +1,14 @@
+using Documenter, StatFiles
+
+makedocs(
+	modules = [StatFiles],
+	sitename = "StatFiles.jl",
+	analytics="UA-132838790-1",
+	pages = [
+        "Introduction" => "index.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/queryverse/StatFiles.jl.git"
+)
